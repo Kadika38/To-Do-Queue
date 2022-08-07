c95e14aba0c0eeaaea361cafd2dfa2a22c62fe66
Kadika <dryan10101@gmail.com>
2022-08-07 04:13:14 -0700

    added todo routes to user routes js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,11 +6,16 @@ const {
     updateUser,
     deleteUser,
     login,
+    /* createTodo,
+    updateTodo,
+    deleteTodo, */
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+/* router.route('/:userId/todo').post(createTodo).put(updateTodo).delete(deleteTodo); */
 
 router.route('/login').post(login);
 
