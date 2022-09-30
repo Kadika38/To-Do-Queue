a4c22d671be2ef905c88ea23029ea33cc1458024
Kadika <dryan10101@gmail.com>
2022-09-30 12:20:01 -0700

    total restart

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-connect('mongodb://localhost/todoDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection;
