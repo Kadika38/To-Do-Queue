4cd33dd8ef36979877a7726d17341fb5ff60615d
Kadika <dryan10101@gmail.com>
2022-09-30 12:48:40 -0700

    added files/file structure to client

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
