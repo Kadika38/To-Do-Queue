fbfdc22595abf3a921206fc5ab7fb413efa0b83e
Kadika <dryan10101@gmail.com>
2022-10-14 17:42:01 -0700

    partially added up down button system to todos

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import "./style.css";
 import Auth from "../../utils/auth";
 import { useQuery } from "@apollo/client";
 import { ONE_USER } from "../../utils/queries";
 import Create from "./components/Create";
-import Upnext from "./components/Upnext";
 import Todo from "./components/Todo";
 
 export default function Main() {
+    const [tq, setTq] = useState([]);
+    const [firstRender, setFirstRender] = useState(true);
     let userId = null;
     if (Auth.loggedIn()) {
         userId = Auth.getUser().data._id;
@@ -36,17 +37,77 @@ export default function Main() {
         )
     }
 
+    const doubleCheckSpots = () => {
+        let copy = JSON.parse(JSON.stringify(tq));
+        copy.sort((a, b) => a.spot-b.spot);
+        for (let i = 0; i < copy.length; i++) {
+            copy[i].spot = i;
+        }
+        if (copy !== tq) {
+            setTq(copy);
+            console.log("copy success");
+        }
+    };
+
     const todos = data.oneUser.todos;
-    const todoQueue = [...todos];
-    todoQueue.sort((a, b) => a.spot-b.spot);
+    let todoQueue = JSON.parse(JSON.stringify(todos));
+    if (firstRender) {
+        setTq(todoQueue);
+        setFirstRender(false);
+        doubleCheckSpots();
+    }
+
+    const shift = (id, direction) => {
+        doubleCheckSpots();
+        let copy = tq;
+        let cont = true;
+        let found = false;
+        if (direction === "up") {
+            for (let i = copy.length-1; i >= 0; i--) {
+                if (found && cont) {
+                    cont = false;
+                    copy[i].spot = (copy[i].spot + 1);
+                }
+                if (!found && cont) {
+                    if (copy[i]._id === id) {
+                        found = true;
+                        copy[i].spot = (copy[i].spot - 1);
+                    }
+                }
+            }
+        }
+        if (direction === "down") {
+            for (let i = 0; i < copy.length; i++) {
+                if (found && cont) {
+                    cont = false;
+                    copy[i].spot = (copy[i].spot - 1);
+                }
+                if (!found && cont) {
+                    if (copy[i]._id === id) {
+                        found = true;
+                        copy[i].spot = (copy[i].spot + 1);
+                    }
+                }
+            }
+        }
+        setTq(copy);
+        doubleCheckSpots();
+        console.log(copy);
+    };
+
+    const save = () => {
+        //later functionality
+    };
+
+    console.log("render");
 
     return (
         <div className="mainContainer">
             <div className="subContainer">
                 <div className="column">
-                    {todoQueue.map((item) => {
+                    {tq.map((item) => {
                         return (
-                            <Todo _id={item._id} title={item.title} repeat={item.repeat} repeatTime={item.repeatTime} creation={item.creation} />
+                            <Todo shift={shift} _id={item._id} title={item.title} repeat={item.repeat} repeatTime={item.repeatTime} creation={item.creation} />
                         );
                     })}
                 </div>
--- client/src/pages/Main/components/Todo.js
+++ client/src/pages/Main/components/Todo.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Todo(props) {
 
-    console.log(props);
-
     return (
         <div className="queue-todo">
             <p>Title: {props.title}</p>
             <p>Repeat? {props.repeat.toString()}</p>
             {props.repeat? <p>RepeatTime: {props.repeatTime}</p> : null}
             <p>Creation: {props.creation}</p>
+            <button onClick={() => props.shift(props._id, "up")}>Up</button>
+            <button onClick={() => props.shift(props._id, "down")}>Down</button>
         </div>
     );
 };
